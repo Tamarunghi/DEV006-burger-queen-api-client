c174fe612e1f50168709cb345f2bcce85bbbe10a
Andrea Roa <andrearoa182@gmail.com>
2023-08-11 19:59:09 -0500

    se agrego alert para cuando se envia un pedido , tambien se bloqueo el boton cuando no se ha llenado el campo de nombre y seleccionado minimo un producto

--- burger-queen/src/01Routes/Waiter.tsx
+++ burger-queen/src/01Routes/Waiter.tsx
@@ -1,41 +1,16 @@
-import React, { useEffect, useState } from "react";
-import dayjs from "dayjs";
-import { GetProducts } from "../02App/getProduct";
-import { PostOrders } from "../02App/postOrders";
-import { OrderSelectionItem } from "../03Components/Waiter/OrderSelectionItem";
-import {
-  AddedToCart,
-  TotalAddedToCart,
-} from "../03Components/Waiter/AddedToCart";
 import { LoggedUserAndExist } from "../03Components/LoggedUserAndExist";
-import { DeletePopup } from "../03Components/Waiter/DeletePopup";
 import { LogoPng } from "../03Components/logoComponent";
 import { Background } from "../03Components/Background";
-import { ICartItems, orderItems, orderData } from "../03Components/Interfaces";
 import { InformationToList } from "../03Components/Waiter/InformationToList";
 import { GetOrders } from "../02App/getOrders";
+import { OrderMenu } from "../03Components/Waiter/OrderMenu";
+import { useEffect, useState } from "react";
 
 export const Waiter: React.FC = () => {
-  const [products, setProducts] = useState<any[]>([]);
   const [productType, setProductType] = useState("Desayuno");
-  const [cartItems, setCartItems] = useState<ICartItems[]>([]);
-  const [customerName, setCustomerName] = useState("");
   const [orders, setOrders] = useState<any[]>([]);
-  //const [customerTable, setcustomerTable] = useState("");
-  useEffect(() => {
-    GetProducts()
-      .then((data) => {
-        const productsWithClick = data.map((product: any) => ({
-          ...product,
-          clicks: 0,
-        }));
-        console.log(data);
-        setProducts(productsWithClick);
-      })
-      .catch((error) => {
-        console.error("Error fetching products", error);
-      });
 
+  useEffect(() => {
     GetOrders()
       .then((data) => {
         setOrders(data); // Guarda las órdenes en el estado 'orders'
@@ -45,123 +20,24 @@ export const Waiter: React.FC = () => {
       });
   }, []);
 
-  const handleSelectProductType = (type: string) => {
-    if (type === "Pedidos") {
-      {
-        orders.map((order) => (
-          <InformationToList
-            id={order.id}
-            key={order.id}
-            client={order.client}
-            products={order.products}
-            dateEntry={order.dateEntry}
-            userId={order.userId}
-            status={order.status}
-          />
-        ));
-      }
-    }
-    setProductType(type);
-  };
-
-  const handleAddToCart = (product: any) => {
-    // Verifica si el producto ya está en el carrito
-    const existingProduct = cartItems.find((item) => item.id === product.id);
-
-    if (existingProduct) {
-      // Si el producto ya está en el carrito, incrementar su contador de clics
-      setCartItems((prevCartItems) => {
-        const updatedCartItems = prevCartItems.map((item) =>
-          item.id === product.id ? { ...item, clicks: item.clicks + 1 } : item
-        );
-        return updatedCartItems;
-      });
+  const renderSelectedComponent = () => {
+    if (productType === "Desayuno" || productType === "Almuerzo") {
+      return <OrderMenu productType={productType} />;
+    } else if (productType === "Pedidos") {
+      return orders.map((order) => (
+        <InformationToList
+          key={order.id}
+          client={order.client}
+          products={order.products}
+          dateEntry={order.dateEntry}
+          userId={order.userId}
+          status={order.status}
+          id={order.id}
+        />
+      ));
     } else {
-      // Si el producto no está en el carrito, agregarlo con un contador de clics inicial de 1
-      setCartItems((prevCartItems) => [
-        ...prevCartItems,
-        { ...product, clicks: 1 },
-      ]);
+      return null; // Return null for other cases
     }
-  };
-  const handleIncremetQuantity = (productId: number) => {
-    setCartItems((prevCartItems) => {
-      const updatedCartItems = prevCartItems.map((item) =>
-        item.id === productId ? { ...item, clicks: item.clicks + 1 } : item
-      );
-      return updatedCartItems;
-    });
-  };
-  const handleDecremetQuantity = (productId: number) => {
-    setCartItems((prevCartItems) => {
-      const updatedCartItems = prevCartItems.map((item) =>
-        item.id === productId
-          ? { ...item, clicks: Math.max(1, item.clicks - 1) }
-          : item
-      );
-
-      return updatedCartItems;
-    });
-  };
-  const handleDeleteCartItem = (productId: number) => {
-    DeletePopup()
-      .then((result) => {
-        if (result.isConfirmed) {
-          setCartItems((prevCartItems) => {
-            const updatedCartItems = prevCartItems.filter(
-              (item) => item.id !== productId
-            );
-            return updatedCartItems;
-          });
-        } else if (result.isDenied) {
-          // El usuario hizo clic en el botón cancelar o cerró el SweetAlert
-          console.log("Eliminación cancelada");
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  const handleSetValue = (
-    callback: (a: string) => void,
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    callback(event.target.value);
-  };
-
-  const handleSendOrders = () => {
-    //   const currentDateFormatted = dayjs().format("YYYY-MM-DD HH:mm:ss");
-    // console.log("Current Date:", currentDateFormatted);
-
-    const orderItems: orderItems[] = cartItems.map((item) => ({
-      qty: item.clicks,
-      product: {
-        id: item.id,
-        name: item.name,
-        price: item.price,
-        type: item.type,
-        dateEntry: item.dateEntry,
-        // imagen: item.image,
-      },
-    }));
-    const orderData: orderData = {
-      userId: localStorage.getItem("userId"),
-      client: customerName,
-      products: orderItems,
-      status: "Pendiente",
-      dateEntry: dayjs().format("YYYY-MM-DD HH:mm:ss"),
-    };
-    console.log(orderData);
-    PostOrders(orderData)
-      .then((response) => {
-        console.log("se guardooo perro", response);
-        return response;
-      })
-      .catch((error) => {
-        7;
-        console.error("error perro", error);
-      });
   };
 
   return (
@@ -193,7 +69,7 @@ export const Waiter: React.FC = () => {
                 ? "textTransform: uppercase"
                 : "bg-yellow" // Aplicamos estilo con bg-yellow si es el tipo de producto seleccionado
             }`}
-            onClick={() => handleSelectProductType("Desayuno")} // Manejador para seleccionar desayuno
+            onClick={() => setProductType("Desayuno")} // Manejador para seleccionar desayuno
           >
             <h1>Desayuno</h1>
           </div>
@@ -203,7 +79,7 @@ export const Waiter: React.FC = () => {
                 ? "textTransform: uppercase"
                 : "bg-yellow" // Aplicamos estilo con bg-yellow si es el tipo de producto seleccionado
             }`}
-            onClick={() => handleSelectProductType("Almuerzo")} // Manejador para seleccionar Almuerzo
+            onClick={() => setProductType("Almuerzo")} // Manejador para seleccionar Almuerzo
           >
             <h1>Almuerzo/Cena</h1>
           </div>
@@ -213,7 +89,7 @@ export const Waiter: React.FC = () => {
                 ? "textTransform: uppercase"
                 : "bg-yellow" // Aplicamos estilo con bg-yellow si es el tipo de producto seleccionado
             }`}
-            onClick={() => handleSelectProductType("Pedidos")} // Manejador para seleccionar Pedidos
+            onClick={() => setProductType("Pedidos")} // Manejador para seleccionar Pedidos
           >
             <h1>pedidos</h1>
           </div>
@@ -224,119 +100,7 @@ export const Waiter: React.FC = () => {
           id="menuYCompra"
           className=" h-[755px] w-[100%] bg-crema p-[20px] rounded-b-[25px] rounded-tl-[25px] overflow-auto"
         >
-          {/* ---Name + Table--- */}
-          <div
-            id="nameAndTable"
-            className="h-[10%] w-[100%] p-[1%] text-brownText text-[1.2rem] font-bold flex flex-row justify-evenly items-center gap-1"
-          >
-            <h1>Nombre:</h1>
-            <input
-              type="text"
-              value={customerName}
-              onChange={(e) => handleSetValue(setCustomerName, e)}
-              className="bg-skin h-[50%] w-[40%] rounded-5"
-            ></input>
-          </div>
-
-          {/* ---Order Selection--- */}
-          <div
-            id="orderSelection"
-            className="h-auto w-[100%] p-[1%] flex flex-wrap justify-center justify-evenly text-center gap-y-4"
-          >
-            {/* ---Menu/Cart--- */}
-            {products
-              .filter((product) => product.type === productType)
-              .map((product) => (
-                <OrderSelectionItem
-                  key={product.id}
-                  name={product.name}
-                  price={product.price}
-                  // image={product.image}
-                  quantity={
-                    cartItems.find((item) => item.id === product.id)?.clicks ||
-                    0
-                  }
-                  onClick={() => {
-                    handleAddToCart(product);
-                  }}
-                />
-              ))}
-          </div>
-
-          {/* ---Shopping Cart--- */}
-
-          <div
-            id="cart"
-            className="h-auto w-[100%] p-[5%] text-[1.5rem] font-bold"
-          >
-            {/* ---Titles--- */}
-            <div
-              id="titles"
-              className="h-[50px] w-[100%] grid grid-cols-10 gap-1 text-center mb-[15px]"
-            >
-              <div
-                id="product"
-                className="bg-yellow col-span-3 rounded-tl-[15px]"
-              >
-                Producto
-              </div>
-              <div id="quantity" className="bg-yellow col-span-4">
-                Cantidad
-              </div>
-              <div
-                id="price"
-                className="bg-yellow col-span-2 rounded-tr-[15px]"
-              >
-                Precio
-              </div>
-              <div id="delete" className="col-span-1"></div>
-            </div>
-
-            {/* ---Products added--- */}
-            {cartItems.map((product) => (
-              <AddedToCart
-                key={product.id}
-                name={product.name}
-                clicks={product.clicks}
-                price={product.price}
-                Increment={() => handleIncremetQuantity(product.id)}
-                Decrement={() => handleDecremetQuantity(product.id)}
-                Delete={() => handleDeleteCartItem(product.id)}
-              />
-            ))}
-            {/* ---Total--- */}
-            <TotalAddedToCart cartItems={cartItems} />
-
-            {productType === "Pedidos" && (
-              <div>
-                {orders.map((order) => (
-                  <InformationToList
-                    id={order.id}
-                    key={order.id}
-                    client={order.client}
-                    products={order.products}
-                    dateEntry={order.dateEntry}
-                    userId={order.userId}
-                    status={order.status}
-                  />
-                ))}
-              </div>
-            )}
-          </div>
-
-          {/* ---Send Buttom--- */}
-          <div
-            id="SendButtom"
-            className="h-[10%] w-[100%] flex justify-center items-center"
-          >
-            <button
-              type="submit"
-              className="bg-colorButton hover:bg-buttonHover h-[65px] w-[500px] items-center rounded-[45px]  font-bold text-brownText text-[1.5rem]"
-              onClick={handleSendOrders}
-            >
-              Enviar a Cocina
-            </button>
-          </div>
+          {renderSelectedComponent()}
         </section>
       </main>
 
